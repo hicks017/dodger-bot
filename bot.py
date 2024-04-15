ea1df77f0dfcc6b73e9f676a8e0da417c0d02e59
Christian <hicks017@cougars.csusm.edu>
2024-04-14 20:23:36 -0700

    Update bot scripts

--- bot.py
+++ bot.py
@@ -1,13 +1,33 @@
 import os
 import discord
+from discord.ext import tasks, commands
 from dotenv import load_dotenv
+from datetime import datetime
+
+bot = commands.Bot(command_prefix = "!", intents = discord.Intents.default())
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-client = discord.Client(intents=discord.Intents.default())
+token = os.getenv("DISCORD_TOKEN")
+# guild_id = int(os.environ.get("GUILD_ID"))
+# guild = bot.get_guild(guild_id)
+channel_id = int(os.environ.get("CHANNEL_ID"))
+channel = bot.get_channel(channel_id)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-client.run(TOKEN)
+@tasks.loop(seconds = 5)
+async def timedMessage():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Connected to loop")
+    await print(f"{current_time}")
+
+@timedMessage.before_loop
+async def before():
+    await bot.wait_until_ready()
+    print("Finished waiting")
+
+timedMessage.start()
+bot.run(token)
