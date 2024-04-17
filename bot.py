8c9f6ff53d292eec09088fa8d44a601889d11b11
Christian <hicks017@cougars.csusm.edu>
2024-04-16 17:45:58 -0700

    Improve script further Define global variable `channel` and populate the value AFTER the bot has initialized. Also use a different function (AsyncIOScheduler) to schedule the bot.

--- bot.py
+++ bot.py
@@ -3,31 +3,31 @@ import discord
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
 from datetime import datetime
+import pytz
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 bot = commands.Bot(command_prefix = "!", intents = discord.Intents.default())
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 # guild_id = int(os.environ.get("GUILD_ID"))
-# guild = bot.get_guild(guild_id)
 channel_id = int(os.environ.get("CHANNEL_ID"))
-channel = bot.get_channel(channel_id)
+channel = None
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    global channel
+    channel = bot.get_channel(channel_id)
+    print(f'{bot.user} has connected to {channel}!')
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(timedMessage, 'cron', hour=17, minute=44, second=0, timezone=pytz.timezone("US/Pacific"))
+    scheduler.start()
 
-@tasks.loop(seconds = 5)
 async def timedMessage():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("Connected to loop")
-    await print(f"{current_time}")
+    if 1 == 1:
+        timezone = pytz.timezone("US/Pacific")
+        now = datetime.now(timezone)
+        current_time = now.strftime("%H:%M:%S")
+        await channel.send("test!!!")
 
-@timedMessage.before_loop
-async def before():
-    await bot.wait_until_ready()
-    print("Finished waiting")
-
-timedMessage.start()
 bot.run(token)
