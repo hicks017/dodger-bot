1f39607781278328a152c5a785b1287a44cb3fbb
Christian <hicks017@cougars.csusm.edu>
2024-04-16 19:05:17 -0700

    Finalize V1.0 Implemented full script and added commentary

--- bot.py
+++ bot.py
@@ -11,29 +11,64 @@ from pybaseball import batting_stats_range
 
 bot = commands.Bot(command_prefix = "!", intents = discord.Intents.default())
 
+# Obtain environment variables defined in .env file
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 # guild_id = int(os.environ.get("GUILD_ID"))
 channel_id = int(os.environ.get("CHANNEL_ID"))
 channel = None
 
+# Define Dodgers team ID from the MLB API
 dodgers = 119
 
+# Define bot startup actions
 @bot.event
 async def on_ready():
+
+    # Confirm bot connection to target Discord channel
     global channel
     channel = bot.get_channel(channel_id)
     print(f'{bot.user} has connected to {channel}!')
+
+    # Define schedule for function timedMessage()
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(timedMessage, 'cron', hour=18, minute=14, second=30, timezone=pytz.timezone("US/Pacific"))
+    scheduler.add_job(timedMessage, 'cron', hour=9, minute=0, second=0, timezone=pytz.timezone("US/Pacific"))
     scheduler.start()
 
+# Setup scheduled bot message
 async def timedMessage():
-    if 1 == 1:
-        today = datetime.date.today()
-        today_minus_10 = today - datetime.timedelta(days = 10)
-        today = today.strftime(format = "%B %-d, %Y")
-        today_minus_10 = today_minus_10.strftime(format = "%B %-d, %Y")
-        await channel.send('today is ' + today + ' and 10 days ago was ' + today_minus_10)
+    
+    # Obtain dates for current day and 10 days ago
+    today = datetime.date.today()
+    today_minus_10 = today - datetime.timedelta(days = 10)
+
+    # Obtain date and team names for next Dodgers' game
+    next_game = next_game(dodgers)
+    next_game_data = boxscore_data(next_game)
+    next_game_teams = sorted((next_game_data["teamInfo"]["away"]["teamName"], next_game_data["teamInfo"]["home"]["teamName"]))
+    next_game_calendar_date = json_normalize(next_game_data["gameBoxInfo"])
+    next_game_calendar_date = next_game_calendar_date[next_game_calendar_date["label"].str.endswith(("2024", "2025", "2026", "2027", "2028", "2029"))]["label"].values[0]
+
+    # Compare today's date to date of next Dodgers' game
+    if today.strftime(format = "%B %-d, %Y") == next_game_calendar_date:
+
+        # Obtain team names for previous Dodgers' game
+        last_game_id = last_game(dodgers)
+        last_game_data = boxscore_data(last_game_id)
+        last_game_teams = sorted((last_game_data["teamInfo"]["away"]["teamName"], last_game_data["teamInfo"]["home"]["teamName"]))
+
+        # Obtain batting stats for Dodgers hitters within last 10 days
+        batting_all = batting_stats_range(today_minus_10.strftime(format = "%Y-%m-%d"), today.strftime(format = "%Y-%m-%d"))
+        batting_dodgers = batting_all[(batting_all["Tm"] == "Los Angeles") & (batting_all["Lev"] == "Maj-NL")].sort_values(by = "BA", ascending = False)
+
+        # Calculate median at-bats
+        batting_abs_median = batting_dodgers["AB"].median()
+
+        # Compile top 3 Dodgers hitters with at least the median at-bats into a data frame
+        batting_dodgers_top_3 = batting_dodgers[batting_dodgers["AB"] >= batting_abs_median][["Name", "BA", "AB", "H", "2B", "3B", "HR"]].head(3).reset_index(drop=True)
+        batting_dodgers_top_3.index = batting_dodgers_top_3.index + 1
+
+        # Send data frame to Discord inside a code block
+        await channel.send('Top 3 hitters for the last 10 days!\n' + '```' + batting_dodgers_top_3.to_string() + '```')
 
 bot.run(token)
