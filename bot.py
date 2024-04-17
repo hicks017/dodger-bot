b4c563bf1210458296fa141a4c7ce11bc0a41716
Christian <hicks017@cougars.csusm.edu>
2024-04-16 18:17:00 -0700

    Print vars in Discord message

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import os
 import discord
-from discord.ext import tasks, commands
-from dotenv import load_dotenv
-from datetime import datetime
+import datetime
 import pytz
+from discord.ext import commands
+from dotenv import load_dotenv
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from statsapi import next_game, last_game, boxscore_data
+from pandas import json_normalize
+from pybaseball import batting_stats_range
 
 bot = commands.Bot(command_prefix = "!", intents = discord.Intents.default())
 
@@ -14,20 +17,23 @@ token = os.getenv("DISCORD_TOKEN")
 channel_id = int(os.environ.get("CHANNEL_ID"))
 channel = None
 
+dodgers = 119
+
 @bot.event
 async def on_ready():
     global channel
     channel = bot.get_channel(channel_id)
     print(f'{bot.user} has connected to {channel}!')
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(timedMessage, 'cron', hour=17, minute=44, second=0, timezone=pytz.timezone("US/Pacific"))
+    scheduler.add_job(timedMessage, 'cron', hour=18, minute=14, second=30, timezone=pytz.timezone("US/Pacific"))
     scheduler.start()
 
 async def timedMessage():
     if 1 == 1:
-        timezone = pytz.timezone("US/Pacific")
-        now = datetime.now(timezone)
-        current_time = now.strftime("%H:%M:%S")
-        await channel.send("test!!!")
+        today = datetime.date.today()
+        today_minus_10 = today - datetime.timedelta(days = 10)
+        today = today.strftime(format = "%B %-d, %Y")
+        today_minus_10 = today_minus_10.strftime(format = "%B %-d, %Y")
+        await channel.send('today is ' + today + ' and 10 days ago was ' + today_minus_10)
 
 bot.run(token)
